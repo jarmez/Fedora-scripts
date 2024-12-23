ad35322942d3d7ccfe935611960054d6882c1314
James Day <52564718+jarmez@users.noreply.github.com>
2024-12-23 15:00:38 +1300

    Add files via upload

--- FedoraAppInstall.sh
+++ FedoraAppInstall.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 #Install apps from base repos
 sudo dnf install thunderbird foliate kitty solaar transmission vlc podman gimp rawtherapee inkscape ardour neovim tmux neofetch
+#obsidian
+flatpak install flathub md.obsidian.Obsidian -y
+#vscodium
+flatpak install flathub com.vscodium.codium -y
+#beekeeper
+flatpak install flathub io.beekeeperstudio.Studio -y
+#pgadmin4
+flatpak install flathub org.pgadmin.pgadmin4 -y
+#notesnook
+flatpak install flathub com.notesnook.Notesnook -y
+#protonmail bridge
+flatpak install flathub ch.protonmail.protonmail-bridge -y
+#protonvpn
+flatpak install flathub com.protonvpn.www -y
+#protonpass
+flatpak install flathub me.proton.Pass -y
 #spotify
-flatpak install flathub com.spotify.Client
+flatpak install flathub com.spotify.Client -y
 #tor
 echo '[tor]
 name=Tor for Fedora $releasever - $basearch
@@ -11,22 +27,8 @@ enabled=1
 gpgcheck=1
 gpgkey=https://rpm.torproject.org/fedora/public_gpg.key
 cost=100' > etc/yum.repos.d/tor.repo
-sudo dnf install tor
-#obsidian
-flatpak install flathub md.obsidian.Obsidian
-#vscodium
-flatpak install flathub com.vscodium.codium
-#beekeeper
-flatpak install flathub io.beekeeperstudio.Studio
-#pgadmin4
-flatpak install flathub org.pgadmin.pgadmin4
-#notesnook
-flatpak install flathub com.notesnook.Notesnook
-#protonmail
-flatpak install flathub ch.protonmail.protonmail-bridge
-#protonvpn
-flatpak install flathub com.protonvpn.www
-#protonpass
-flatpak install flathub me.proton.Pass
+sudo dnf install tor -y
+
+
 
 
