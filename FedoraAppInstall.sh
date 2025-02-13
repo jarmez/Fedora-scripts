780ec3ff9c834d065452808d40b5744bf77223e8
James Day <52564718+jarmez@users.noreply.github.com>
2025-02-13 21:26:29 +0000

    Update FedoraAppInstall.sh

--- FedoraAppInstall.sh
+++ FedoraAppInstall.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #Install apps from base repos
-sudo dnf install thunderbird foliate kitty solaar transmission vlc podman gimp rawtherapee inkscape ardour neovim tmux neofetch
+sudo dnf install thunderbird foliate kitty solaar transmission vlc podman gimp rawtherapee inkscape neovim fastfetch
+
+#Install flatpak apps
 #obsidian
 flatpak install flathub md.obsidian.Obsidian -y
 #vscodium
 flatpak install flathub com.vscodium.codium -y
-#beekeeper
-flatpak install flathub io.beekeeperstudio.Studio -y
-#pgadmin4
-flatpak install flathub org.pgadmin.pgadmin4 -y
-#notesnook
-flatpak install flathub com.notesnook.Notesnook -y
 #protonmail bridge
 flatpak install flathub ch.protonmail.protonmail-bridge -y
 #protonvpn
@@ -19,15 +15,7 @@ flatpak install flathub com.protonvpn.www -y
 flatpak install flathub me.proton.Pass -y
 #spotify
 flatpak install flathub com.spotify.Client -y
-#tor
-echo '[tor]
-name=Tor for Fedora $releasever - $basearch
-baseurl=https://rpm.torproject.org/fedora/$releasever/$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://rpm.torproject.org/fedora/public_gpg.key
-cost=100' > etc/yum.repos.d/tor.repo
-sudo dnf install tor -y
+
 
 
 
